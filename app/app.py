0fd0b6343ee431034ca8664b7e308e2085eeb648
Guillaume Azerad <guillaume.azerad@gmail.com>
2025-01-01 10:33:02 +0100

    Add of playwright testing feature

--- app/app.py
+++ app/app.py
@@ -87,6 +87,24 @@ def decrement_counter():
         conn.close()
     return jsonify({"value": updated_value}), 200
 
+@app.route('/counter/reset', methods=['PUT'])
+def reset_counter():
+    """Reset the counter."""
+    conn, cursor, status = set_db_conn_cursor()
+    if not status:
+        return jsonify({"error": "Database connection failure"}), 500
+    try:
+        cursor.execute('UPDATE counter SET value = 0 WHERE id = 1 RETURNING value')
+        updated_value = cursor.fetchone()[0]
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        return jsonify({"error": "Counter reset query failure"}), 500
+    finally:
+        cursor.close()
+        conn.close()
+    return jsonify({"value": updated_value}), 200
+
 def handler(event, context):
     """Lambda handler when executed in an AWS Lambda function"""
     return awsgi2.response(app, event, context)
